860e49419b5ca4e22b93761335d8470954885574
yGaisa <150799082+yGaisa@users.noreply.github.com>
2024-08-19 14:02:06 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE teste (
+  	ID integer primary key autoincrement,
+  	nome varchar(10) not null,
+  	descricao text, 
+  	preco decimal(10,2),
+  	dataHora datetime default current_timestamp 
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
